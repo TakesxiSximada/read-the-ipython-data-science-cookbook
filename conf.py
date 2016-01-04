c8a96145d96caf06fa8aa49bca7a962b53c1cf29
TakesxiSximada <takesxi.sximada@gmail.com>
2016-01-05 00:28:13 +0900

    for markdown

--- conf.py
+++ conf.py
@@ -37,8 +37,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
@@ -359,3 +358,7 @@ epub_exclude_files = ['search.html']
 
 # If false, no index is generated.
 # epub_use_index = True
+
+source_parsers = {
+    '.md': 'sphinxcontrib.markdown.MarkdownParser',
+}
